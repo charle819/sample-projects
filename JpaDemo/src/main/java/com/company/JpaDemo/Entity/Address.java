1b461f72ba73413b75b7ef476ef2ea51fc57b6b1
Chinmay Purohit <chinmay.purohit@basilroot.com>
2017-10-10 14:06:21 +0530

    adding final project commit

---
+++ JpaDemo/src/main/java/com/company/JpaDemo/Entity/Address.java
@@ -0,0 +1,52 @@
+package com.company.JpaDemo.Entity;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="address")
+public class Address {
+
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY)
+	private int id;
+	private String street;
+	private String city;
+	private String country;
+	private String postalcode;
+	public int getId() {
+		return id;
+	}
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getStreet() {
+		return street;
+	}
+	public void setStreet(String street) {
+		this.street = street;
+	}
+	public String getCity() {
+		return city;
+	}
+	public void setCity(String city) {
+		this.city = city;
+	}
+	public String getCountry() {
+		return country;
+	}
+	public void setCountry(String country) {
+		this.country = country;
+	}
+	public String getPostalcode() {
+		return postalcode;
+	}
+	public void setPostalcode(String postalcode) {
+		this.postalcode = postalcode;
+	}
+	
+	
+}
